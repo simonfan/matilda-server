980d53d7372315118bd03e827cbc7f1329afc147
Simon <simonxd@gmail.com>
2017-02-18 11:20:32 -0200

    styles and keeping track of last messages

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const path = require('path');
 const express  = require('express');
 const socketIO = require('socket.io');
 
+// constants
+const KEEP_MESSAGES_COUNT = 10;
+const EVENT_NAME = 'event';
+
 var app = express();
  
 app.use('/static', express.static('static'));
@@ -13,11 +17,35 @@ app.use('/static', express.static('static'));
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on('connection', function(client){
-  client.on('event', function (data) {
+/**
+ * Keeping track of last events
+ * @type {Array}
+ */
+var lastEvents = [];
+
+function addEventToLastEvents(data) {
+  if (lastEvents.length >= KEEP_MESSAGES_COUNT) {
+    // remove the oldest message
+    lastEvents.shift();
+  }
+
+  lastEvents.push(data);
+}
+
+io.on('connection', function(client) {
+
+  client.on(EVENT_NAME, function (data) {
     console.log('received event (will broadcast)', data);
-    client.broadcast.emit('event', data);
+    addEventToLastEvents(data);
+    client.broadcast.emit(EVENT_NAME, data);
   });
+
+  // send client last events
+  lastEvents.forEach((data) => {
+    client.emit(EVENT_NAME, data);
+  });
+
+
   client.on('disconnect', function(){});
 });
 
